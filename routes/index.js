90e6b120aa288180a71b8e41bbf8fe43fb1cc575
cesar <cesar@sumawealth.com>
2022-10-26 11:16:02 -0700

    add get handler to check api status

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 const SendNotification = require('../lib/SendNotification');
 var router = express.Router();
 
-/* GET home page. */
+/* GET the app status */
+router.get('/', async function(req, res, next) {
+  res.json({status: "online"});
+});
+
+/* POST an insight to homepage */
 router.post('/', async function(req, res, next) {
   
   const response = await SendNotification(req.body)
